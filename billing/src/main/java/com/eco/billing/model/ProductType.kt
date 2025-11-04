2f0495697b21b7e141629eb7081f4c9c05522636
lehuong0150 <lehuong1050@gmail.com>
2025-11-04 08:30:02 +0700

    delete billing

--- billing/src/main/java/com/eco/billing/model/ProductType.kt
+++
@@ -1,6 +0,0 @@
-package com.eco.billing.model
-
-import com.android.billingclient.api.BillingClient
-
-const val SUBS = BillingClient.ProductType.SUBS
-const val IN_APP = BillingClient.ProductType.INAPP
